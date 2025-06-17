ffccd9799d619ac35015a5a9e6ae1c9d227b114a
root2irrational <newaskershardul@gmail.com>
2025-06-17 22:22:04 +1000

    dih

--- Regressor/pred.py
+++ Regressor/pred.py
@@ -7,8 +7,7 @@ from findPairs import data, nInst, nt
 stock = 2
 best_stock = -1
 
-log_r = np.copy(log_returns[stock])   # <-- Replace with your 1D log return array
-# log_r_sd = np.std(log_r)
+log_r = np.copy(log_returns[stock])
 predicted = np.zeros((nt,1))
 prices = np.copy(data[stock])
 season_trend = 5
@@ -23,12 +22,9 @@ def predError(stock):
   prices = np.copy(data[stock])
   for i in range(start, nt):
     window = log_r[i - season_trend:i - 1]
-    # alpha = 0.999  # decay factor
-    # weights = alpha ** np.arange(season_trend - 1, -1, -1)
     weights = np.linspace(1, 2, season_trend - 1)
     weighted_avg = np.average(window, weights=weights)
-    #predicted[i] = prices[i - 1] * np.mean(np.power(np.exp(log_r[i - season_trend:i]), 1/2))
-    predicted[i] = prices[i - 1] * np.exp(weighted_avg / 1) ### with this prediction is 1 day ahead officially
+    predicted[i] = prices[i - 1] * np.exp(weighted_avg / 10) ###
   percentage_error = np.abs((predicted[start:end] - prices[start:end]) / prices[start:end]) * 100
   return np.mean(percentage_error)
 
@@ -53,7 +49,6 @@ print(stocksRankByMAPE)
 best_stock = 6
 prices = np.copy(data[best_stock])
 
-# start, end = season_trend + 1, nt
 print("\nFor stock "  + str(best_stock) + ":\n")
 print("MAPE:", predError(best_stock), "%") #Mean Absolute Percentage Error
 mse = np.mean((predicted[start:end] - prices[start:end])**2)
@@ -65,7 +60,8 @@ for i in range(290, 301):
   print("Pred: " + str(predicted[i + 1]) + " Actual: " + str(prices[i]))
 
 x = np.arange(start, end)
-plt.plot(x, predicted[start + 1:end + 1], label=' Predicted') ## predicted price is lagging 1 day ahead of actual so to check visually i did + 1
+plt.plot(x, predicted[start + 1:end + 1], label=' Predicted') 
+## predicted price is lagging 1 day ahead of actual so to check visually i did + 1
 plt.plot(x, prices[start:end], label='Actual')
 plt.legend()
 plt.title('Price vs day')
